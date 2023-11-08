89c7d4a483a7e507904ed8595cc8519a2e69e4b4
Sergio <bs.sergiocalderon@gmail.com>
2023-11-08 16:00:33 -0300

    Comentarios y borrado de replaceMonto

--- web-admin-app/backend/controllers/cuentaController.js
+++ web-admin-app/backend/controllers/cuentaController.js
@@ -5,6 +5,7 @@ const tarjetaModel = require('../models/card')
 // Definir controlador (funciones disponibles)
 const controller = {
 
+    // Devuelve el monto de la cuenta asociada al número de tarjeta indicado
     getMonto: async(req, res) => {
         const tarjetaNro = req.params.tarjeta
 
@@ -13,6 +14,7 @@ const controller = {
         return monto ? res.status(200).json(monto) : res.status(400).json({message: "No se encontró cuenta"})
     },
 
+    // Suma el monto especificado a la cuenta asociada a la tarjeta indicada
     ingresarMonto: async(req, res) => {
         const {tarjetaNro, monto} = req.body
 
@@ -24,30 +26,17 @@ const controller = {
         return doc ? res.status(200).json({monto: doc.monto}) : res.status(400).json({message: "Error al actualizar monto"})
     },
 
+    // Resta el monto especificado a la cuenta asociada a la tarjeta indicada
     retirarMonto: async(req, res) => {
         const {tarjetaNro, monto} = req.body
 
         const tarjeta = await tarjetaModel.findOne({nro: tarjetaNro})
         const cuenta = await model.findOne({tarjeta: tarjeta._id})
         const nuevoMonto = parseInt(cuenta.monto) - parseInt(monto)
-        if (nuevoMonto< 0 ){
-            return res.status(400).json({message: "Error en retiro saldo mayor al actual"})
-        }
+        if (nuevoMonto < 0) return res.status(400).json({message: "Error en retiro saldo mayor al actual"})
 
         const doc = await model.findByIdAndUpdate(cuenta._id, {monto: nuevoMonto}, {new: true})
         return doc ? res.status(200).json({monto: doc.monto}) : res.status(400).json({message: "Error al actualizar monto"})
-    },
-
-    replaceMonto: async(req, res) => {
-        const cuentaId = req.params.id
-        const nuevo_monto = req.params.monto
-
-        const cuenta = await model.findById(cuentaId);
-        if (!cuenta) return res.status(400).json({message: "No se encontró la cuenta en el sistema"})
-
-        const doc = await model.findByIdAndUpdate(cuentaId, {monto: nuevo_monto}, {new: true})
-        const result = await doc.save();
-        return result ? res.json(result) : res.status(400).json({message: "Error al actualizar"});
     }
 
 }
