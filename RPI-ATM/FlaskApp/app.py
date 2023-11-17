123b6cd289ad386628af490d9f51750ef0dcbd35
Sergio <bs.sergiocalderon@gmail.com>
2023-11-16 21:38:13 -0300

    Menu y cierre de sesión

--- RPI-ATM/FlaskApp/app.py
+++ RPI-ATM/FlaskApp/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, jsonify, redirect, request
+from enum import Enum                   # Enumerativos
 from time import sleep
 import threading
 
+class Estados(Enum):
+    MENU = 0
+    ESPERANDO_TARJETA = 1
+    CONOCIENDO_PIN = 2
+    INGRESO_PIN = 3
+
 app = Flask(__name__)
-card_read = False
+current_state = Estados.ESPERANDO_TARJETA
 
 DEFAULT_PIN = "1234"
 
@@ -11,43 +18,67 @@ DEFAULT_PIN = "1234"
 
 @app.route("/")
 def home():
-    return redirect('pin-input' if card_read else 'waiting-card')
+    if current_state == Estados.ESPERANDO_TARJETA:
+        return redirect('waiting-card')
+    if current_state == Estados.INGRESO_PIN: 
+        return redirect('pin-input')
+    if current_state == Estados.MENU:
+        return redirect('menu')
 
 @app.route("/waiting-card")
 def waiting_card():
-    return render_template('waiting_card.html', message="Esperando tarjeta...")
+    if (current_state != Estados.ESPERANDO_TARJETA):
+        return redirect('/')
+    else:
+        return render_template('waiting_card.html', message="Esperando tarjeta...")
 
 @app.route("/pin-input")
 def pin_input():
-    if not card_read:
+    if (current_state != Estados.INGRESO_PIN):
         return redirect('/')
     else:
         return render_template('pin_input.html', message="Ingrese PIN")
     
 @app.route("/menu")
 def menu():
-    return render_template('menu.html')
+    if (current_state == Estados.MENU):
+        return render_template('menu.html')
+    else:
+        return redirect('/')
+    
+@app.route("/finish-session")
+def finish_session():
+    global current_state
+    current_state = Estados.ESPERANDO_TARJETA
+    return redirect("/")
 
 # ---- API REQUESTS --------------------------------------
 
 @app.route("/status")
 def status():
-    return jsonify(dict(status=('ready' if card_read else 'waiting')))
+    return jsonify(dict(status=('ready' if current_state == Estados.INGRESO_PIN else 'waiting')))
 
 @app.route("/pin-process", methods=['POST'])
 def pin_process():
+    global current_state
     data = request.get_json()
     pin_ingresado = data['pin']
-    print("PIN ingresado:", pin_ingresado)
-    return jsonify(result = "1" if pin_ingresado == DEFAULT_PIN else "0")
+    
+    if pin_ingresado == DEFAULT_PIN:
+        current_state = Estados.MENU
+        return jsonify(result = "1")
+    else:
+        return jsonify(result = "0")
 
 # ---- TAREAS DE SEGUNDO PLANO -------------------
 
 def backgroundLoop():
-    global card_read
-    sleep(15)
-    print("card is now read")
-    card_read = True
+    global current_state
+
+    while 1:
+        sleep(8)
+        if current_state == Estados.ESPERANDO_TARJETA: 
+            current_state = Estados.INGRESO_PIN
 
 # -------------------------------------------------
 
@@ -57,4 +88,4 @@ if __name__ == "__main__":
 
     # Hacerlo accesible desde otros dispositivos
     # Si debug se deja en True, se crean 2 background threads
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ RPI-ATM/FlaskApp/static/js/menu.js
@@ -0,0 +1,7 @@
+// botones
+const terminar_btn = document.getElementById("terminar-btn")
+
+// listeners
+function finish_session(){
+    window.location.replace("finish-session")
+}
